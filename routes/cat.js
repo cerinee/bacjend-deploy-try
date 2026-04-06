ab5147562455b1b15328f5058ca08279f1b5a5f7
cerine.c <bendjennicerine@gmail.com>
2026-04-06 15:41:30 +0100

    neww

--- routes/cat.js
+++ routes/cat.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const router = express.Router();
 const { getAllCategories, addCategory, getCategoryById, DeleteById, addMealToCategory} = require('../controllers/catController');
 
--- routes/img.js
+++ routes/img.js
@@ -1,3 +1,4 @@
+const express = require("express")
 const router = express.Router()
 const {addImageToCategory, addImageToMeal, deleteImageFromCategory, deleteImageFromMeal} = require("../controllers/imgController")
 const upload = require("../middleware/multer")
